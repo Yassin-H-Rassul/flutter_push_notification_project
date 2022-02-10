3ca037bce0ea19ddd00a6dc8cd204654747252cc
Yassin Rassul <yassinhrassul@gmail.com>
2022-02-10 17:33:59 +0300

    initial commit

---
+++ android/app/src/main/kotlin/com/example/challenge_in_session/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.challenge_in_session
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:challenge_in_session/services/authService.dart';
+import 'package:challenge_in_session/tokens_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp().then((value) => print('firebase succeedeed.'));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => AuthService()),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  Provider.of<AuthService>(context, listen: false)
+                      .signInAnonymously();
+                  Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) => TokensScreen()));
+                },
+                child: Text("login anonymously"),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
